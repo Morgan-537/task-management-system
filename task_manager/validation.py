510101baccf848166420bf49f4629ac960b7a01d
Morgan Ombati <morgan.ombati@student.moringaschool.com>
2026-06-04 23:56:54 +0300

    Task Manegement complish

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -1,27 +1,21 @@
 from datetime import datetime
 
-
 def validate_task_title(title):
-
-    if len(title) == 0:
+    if len(title.strip()) < 1:
+        print("Invalid task title.")
         return False
-
     return True
-
 
 def validate_task_description(description):
-
-    if len(description) == 0:
+    if len(description.strip()) < 1:
+        print("Invalid task description.")
         return False
-
     return True
 
-
 def validate_due_date(due_date):
-
     try:
         datetime.strptime(due_date, "%Y-%m-%d")
         return True
-
     except ValueError:
+        print("Invalid due date. Use YYYY-MM-DD.")
         return False
